5b4b8bbc94ff7fbc59dbeccb04b1b73921666e49
Manki <damianorzusa2016@gmail.com>
2023-09-26 17:19:34 -0300

    style: alt text img

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,7 @@
 const Card = () => {
   return (
     <div className='bg-white w-80 p-4 rounded-2xl shadow-lg flex flex-col justify-between font-outfit'>
-      <img src='image-qr-code.png' className='rounded-2xl mb-3'/>  
+      <img src='image-qr-code.png' className='rounded-2xl mb-3' alt='qr-code'/>  
       <h3 className='p-3 pb-0 font-extrabold text-xl text-center text-heading lg:text-xl'>Improve your front-end skills by building projects</h3>
       <p className='p-3 text-md text-center text-paragraph pb-5'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>    
     </div>
